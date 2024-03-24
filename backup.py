744751c3388758a73b7200b69adffc5626ae62d6
changwoo <cs.chwnam@gmail.com>
2024-03-24 17:13:11 +0900

    서버 설정 백업 기능 추가

--- backup.py
+++ backup.py
@@ -1,9 +1,10 @@
+import sys
 from datetime import datetime
 from json import loads
-from os import access, X_OK, chdir, getenv, R_OK, getcwd, remove, system
+from os import access, X_OK, chdir, getenv, R_OK, getcwd, remove, system, listdir, mkdir
 from os.path import basename, dirname, exists, expanduser, isdir, isfile, join
 from re import compile
-from shutil import which
+from shutil import which, copy, rmtree
 from subprocess import run, PIPE
 from sys import exit, stderr
 from tempfile import gettempdir
@@ -14,20 +15,19 @@ from dotenv import load_dotenv
 
 class EnvCheck(object):
     def __init__(self):
-        self.git = ''
+        self.conf_remote_path = ''
+        self.conf_targets = []
+        self.conf_capacity = 0
+
         self.gzip = ''
         self.mysqldump = ''
         self.rclone = ''
         self.tar = ''
         self.wp_cli = ''
-        self.wp_root = ''
-        self.remote_path = ''
-        self.remote_capacity = 0
 
-    def test_git(self) -> None:
-        self.git = self.which('git')
-        if not self.git:
-            raise FileNotFoundError('git')
+        self.wp_root = ''
+        self.wp_remote_path = ''
+        self.wp_remote_capacity = 0
 
     def test_gzip(self) -> None:
         self.gzip = self.which('gzip')
@@ -75,7 +75,7 @@ class EnvCheck(object):
 
     @staticmethod
     def test_file(path: str | None) -> None:
-        if exists(path) and isfile and access(path, R_OK):
+        if exists(path) and isfile(path) and access(path, R_OK):
             return
         raise FileNotFoundError(path)
 
@@ -122,12 +122,12 @@ def backup_wordpress() -> None:
     print('Upload to OneDrive ...')
 
     # Snapshot
-    destination = join(env.remote_path, basename(snap_path))
+    destination = join(env.wp_remote_path, basename(snap_path))
     command = '{} copyto {} {}'.format(env.rclone, snap_path, destination)
     env.exec(command)
 
     # DB
-    destination = join(env.remote_path, basename(dump_path))
+    destination = join(env.wp_remote_path, basename(dump_path))
     command = '{} copyto {} {}'.format(env.rclone, dump_path, destination)
     env.exec(command)
 
@@ -140,19 +140,99 @@ def backup_wordpress() -> None:
 
 
 def backup_configs() -> None:
-    pass
+    env = initialize()
+
+    # Get file names.
+    tempdir = gettempdir()
+    dest_dir = join(tempdir, 'conf')
+    mkdir(dest_dir)
+
+    with open(join(dest_dir, 'README.txt'), 'w') as fp:
+        fp.write("Configuration Backup\n")
+        fp.write("====================\n")
+        fp.write("Backup date: " + datetime.now().strftime('%Y-%m-%d') + '\n')
+
+        for src in env.conf_targets:
+            src = expanduser(src)
+            if not src or not exists(src):
+                continue
+
+            if isfile(src) and access(src, R_OK):
+                print('Copying {}'.format(src))
+                copy(src, join(dest_dir, basename(src)))
+                fp.write(src + '\n')
+
+        fp.write("E.O.D.\n")
+
+    cwd = getcwd()
+    chdir(tempdir)
+    archive_path = 'conf-' + datetime.now().strftime('%Y%m%d') + '.tar.gz'
+
+    print('Creating tarball of configurations ...')
+    command = '{} -czf {} {}'.format(env.tar, archive_path, basename(dest_dir))
+    env.exec(command)
+
+    print('Upload to OneDrive ...')
+    destination = join(env.conf_remote_path, archive_path)
+    command = '{} copyto {} {}'.format(env.rclone, archive_path, destination)
+    env.exec(command)
+
+    chdir(cwd)
+    remove(join(tempdir, archive_path))
+    rmtree(dest_dir)
 
 
-def backup_all() -> None:
-    backup_wordpress()
-    backup_configs()
+def limit_conf_backup() -> None:
+    env = initialize()
+    exp = compile(r'conf-\d{8}\.tar\.gz$')
+
+    command = '{} lsjson {}'.format(env.rclone, env.wp_remote_path)
+    code, output = env.exec(command)
+
+    files: List[Tuple[str, datetime]] = []
+
+    if 0 != code:
+        print('The command returned non-zero value: ' + command, file=stderr)
+        exit(1)
+
+    items = loads(output)
+
+    for item in items:
+        is_dir = item['IsDir']
+        mime_type = item['MimeType']
+        path = item['Path']
+        matched = exp.match(path)
+
+        if is_dir or 'application/x-gzip' != mime_type or not matched:
+            continue
+
+        mtime = datetime.fromisoformat(item['ModTime'].replace('Z', '+00:00'))
+        files.append((path, mtime))
+
+    # Sort by mtime
+    files.sort(key=lambda x: x[1], reverse=True)
+
+    to_remove: List[str] = []
+
+    if env.conf_capacity < len(files):
+        exceeded = files[env.conf_capacity:]
+        for item in exceeded:
+            to_remove.append(item[0])
+
+    if to_remove:
+        print('There are more files than capacity. Let\'s remove them.')
+        for item in to_remove:
+            command = '{} delete {}'.format(env.rclone, join(env.wp_remote_path, item))
+            env.exec(command)
+    else:
+        print('Capacity not exceeded. Okay then.')
 
 
-def limit_backup() -> None:
+def limit_wp_backup() -> None:
     env = initialize()
     exp = compile(r'^wp(db|snap)_\d{8}\.(?:tar|sql)\.gz$')
 
-    command = '{} lsjson {}'.format(env.rclone, env.remote_path)
+    command = '{} lsjson {}'.format(env.rclone, env.wp_remote_path)
     code, output = env.exec(command)
 
     snaps: List[Tuple[str, datetime]] = []
@@ -188,20 +268,20 @@ def limit_backup() -> None:
 
     to_remove: List[str] = []
 
-    if env.remote_capacity < len(dbs):
-        exceeded = dbs[env.remote_capacity:]
+    if env.wp_remote_capacity < len(dbs):
+        exceeded = dbs[env.wp_remote_capacity:]
         for item in exceeded:
             to_remove.append(item[0])
 
-    if env.remote_capacity < len(snaps):
-        exceeded = snaps[env.remote_capacity:]
+    if env.wp_remote_capacity < len(snaps):
+        exceeded = snaps[env.wp_remote_capacity:]
         for item in exceeded:
             to_remove.append(item[0])
 
     if to_remove:
         print('There are more files than capacity. Let\'s remove them.')
         for item in to_remove:
-            command = '{} delete {}'.format(env.rclone, join(env.remote_path, item))
+            command = '{} delete {}'.format(env.rclone, join(env.wp_remote_path, item))
             env.exec(command)
     else:
         print('Capacity not exceeded. Okay then.')
@@ -211,17 +291,25 @@ def initialize() -> EnvCheck:
     env_check = EnvCheck()
 
     try:
-        env_check.test_git()
+        env_check.conf_remote_path = getenv('CONF_REMOTE_PATH')
+
+        for target in getenv('CONF_TARGETS').split('\n'):
+            target = target.strip()
+            if target:
+                env_check.conf_targets.append(target)
+
+        env_check.conf_capacity = int(getenv('CONF_CAPACITY')) if getenv('CONF_CAPACITY') else 5
+
         env_check.test_gzip()
         env_check.test_mysqldump()
         env_check.test_rclone()
         env_check.test_tar()
         env_check.test_wp_cli()
-        env_check.test_wp_root(expanduser(getenv('WP_ROOT')))
-        env_check.remote_path = getenv('REMOTE_PATH')
 
-        capacity = getenv('REMOTE_CAPACITY')
-        env_check.remote_capacity = int(capacity) if capacity else 5
+        env_check.test_wp_root(expanduser(getenv('WP_ROOT')))
+        env_check.wp_remote_path = getenv('WP_REMOTE_PATH')
+
+        env_check.wp_remote_capacity = int(getenv('WP_REMOTE_CAPACITY')) if getenv('WP_REMOTE_CAPACITY') else 5
     except FileNotFoundError as err:
         print(basename(err.args[0]) + ' not found!')
         exit(1)
@@ -231,5 +319,9 @@ def initialize() -> EnvCheck:
 
 if '__main__' == __name__:
     load_dotenv()
-    backup_all()
-    limit_backup()
+
+    backup_wordpress()
+    limit_wp_backup()
+
+    backup_configs()
+    limit_conf_backup()
